d527c8cb39ab376b3aa28e6f2fac5613a3ae0774
Pierre BRENGUES <brengues@hotmail.fr>
2017-05-22 15:19:38 +0200

    TP3 1ere pageJSP

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,21 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 public class ListerCollaborateursController extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		// récupère la valeur d'un paramètre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 
-		// récupère la valeur d'un paramètre dont le nom est departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
+	ServletException, IOException {
 
-		// code HTML écrit dans le corps de la réponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-			+ "<ul>"
-			+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-			+ "<li>departement="+ departementParam + "</li>"
-			+ "</ul>");
+		// A ajouter
+
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
+
+		.forward(req, resp);
 
 	}
 
